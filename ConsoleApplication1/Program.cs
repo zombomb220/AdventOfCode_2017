780ec7117d8304a5f4d8790c4248e918b0b8bf46
zombomb220 <brwabiz@brwallc.com>
2017-12-01 14:05:40 -0800

    Initial commit of day 1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1.1: " + Day1.FirstProblem.Run());
+            Console.WriteLine("Day 1.2: " + Day1.SecondProblem.Run());
+            Console.Read();
+        }
+    }
+}
